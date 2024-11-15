7013c76a26b70fefe71ccc5ff06d13f82acadab7
Dreez-Web <alpuis.andres@gmail.com>
2024-11-15 00:20:14 -0300

    Creacion de archivo conexion

---
+++ Conexion.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+class CConecxion:
+    
+    def ConexionBaseDeDatos():
+        try:
+            conexion = mysql.connector.connect(
+                user='root',
+                password='andres',
+                host='localhost',
+                database='clientesdb',
+                port='3306'
+            )
+            print("Conexion exitosa")
+            return conexion
+        
+        except mysql.connector.Error as error:
+            print("Error al conectarse a la base de datos {}".format(error))
+            
+    ConexionBaseDeDatos()
